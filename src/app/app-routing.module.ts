f48708f07dda9f09dd4b9af6d55b28cf526d3d8a
Sagar <sagarbelotes@gmail.com>
2021-03-23 17:09:43 +0530

    angular bookstore assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { CartComponent } from './cart/cart.component';
+const routes: Routes = [
+  {
+    path: 'homepage', component: HomepageComponent, children: [{
+      path: 'products', component: ProductsComponent
+    }, {
+      path: 'inventory', component: InventoryComponent
+    },
+    {
+      path: 'cart', component: CartComponent
+    }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { CartComponent } from './cart/cart.component';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { IndexedDBService } from './indexed-db.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductsComponent,
+    InventoryComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [IndexedDBService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
