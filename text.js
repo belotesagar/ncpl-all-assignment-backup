cadefe97392f532caf4da5d89cefd9ab8e2d17dc
Sagar <sagarbelotes@gmail.com>
2021-03-24 09:24:03 +0530

    index db assignment

---
+++ text.js
@@ -0,0 +1,66 @@
+const dbName = "master_books";
+
+var request = indexedDB.open(dbName, 2);
+var db;
+
+request.onerror = function(event) {
+    console.log("books not created");
+};
+
+request.onsuccess = function(event) {
+    db = request.result;
+    console.log("book object store created successfully");
+}
+
+var bookData = [{
+        id:1,
+        name: 'Politics of Opportunism',
+        autherName: 'R P N Singh',
+        price: 300,
+        language: 'English'
+    },
+    {   id:2,
+        name: 'Obama: The Call of History',
+        autherName: 'Peter Baker',
+        price: 700,
+        language: 'English'
+    },
+    {   id:3,
+        name: 'Mrityunjaya',
+        autherName: 'Shivaji Sawant',
+        price: 945,
+        language: 'Marathi'
+    },
+    {   id:4,
+        name: 'Musafir',
+        autherName: 'Achyut Godbole',
+        price: 1250,
+        language: 'Marathi'
+    },
+    {   id:5,
+        name: 'Maharana Pratap',
+        autherName: 'Kunwar Ishwar Singh Rathore',
+        price: 495,
+        language: 'Hindi'
+    }
+];
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    var objectStore = db.createObjectStore("cart", {
+        keyPath: "id", autoIncrement: true
+    });
+
+    objectStore.createIndex("name", "name", {
+        unique: true
+    });
+
+    objectStore.transaction.oncomplete = function(event) {
+        console.log("books database transaction is complete");
+        var customerObjectStore = db.transaction("cart", "readwrite").objectStore("cart");
+        bookData.forEach(function(book) {
+            customerObjectStore.add(book);
+        });
+    };
+
+};
